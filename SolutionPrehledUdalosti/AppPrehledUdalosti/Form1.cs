2ec03c258398144b91aeca0662f93bfc4a0dd16a
Anh Tuan Nguyen <tuan.anh.nguyen@plus4u.net>
2020-10-09 09:09:59 +0200

    AppPrehledUdalosti

---
+++ SolutionPrehledUdalosti/AppPrehledUdalosti/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AppPrehledUdalosti
+{
+  public partial class Form1 : Form
+  {
+    public Form1()
+    {
+      InitializeComponent();
+    }
+
+    private void button1_Click(object sender, EventArgs e)
+    {
+      System.Reflection.Assembly assembly = System.Reflection.Assembly.GetExecutingAssembly();
+      System.Diagnostics.FileVersionInfo fvi = System.Diagnostics.FileVersionInfo.GetVersionInfo(assembly.Location);
+
+      System.Windows.Forms.MessageBox.Show($"AppPrehledUdalosti Ver={fvi.FileVersion}{Environment.NewLine}" +
+                                          $"{Environment.NewLine}Using {LibUdalostiCore.UdalostiCore.GetMyVersion()}"
+                                          );
+    }
+  }
+}
